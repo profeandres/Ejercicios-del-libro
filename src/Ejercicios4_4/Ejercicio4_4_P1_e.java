99d3c9df0ec5395aefcff1b49464923d84913ea3
Andres Cantillo <andrescantillo@hotmail.com>
2021-07-08 17:09:16 -0500

    Ejercicio Parte 1 e) hecho con redondeo 4 cifras y simplificado

---
+++ src/Ejercicios4_4/Ejercicio4_4_P1_e.java
@@ -0,0 +1,9 @@
+package Ejercicios4_4;
+/*
+Construir un programa que muestre en pantalla la tangente de 60
+*/
+public class Ejercicio4_4_P1_e {
+    public static void main(String[] args) {
+        System.out.println("la tangente comprimida de 60° es: "+ (double) Math.round(Math.tan(60)*10000)/10000);
+    }
+}
