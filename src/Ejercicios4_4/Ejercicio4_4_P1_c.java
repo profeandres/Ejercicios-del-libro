878ec1a8d62bdbe8df3bb0c2d32f1271004771c4
Andres Cantillo <andrescantillo@hotmail.com>
2021-07-08 14:24:17 -0500

    Ejercicio Parte 1 c) Hecho

---
+++ src/Ejercicios4_4/Ejercicio4_4_P1_c.java
@@ -0,0 +1,9 @@
+package Ejercicios4_4;
+/*
+Construir un programa que muestre en pantalla el seno de 45 
+*/
+public class Ejercicio4_4_P1_c {
+    public static void main(String[] args) {
+        System.out.println("El seno de 45° es: "+ Math.sin(45));
+    }
+}
